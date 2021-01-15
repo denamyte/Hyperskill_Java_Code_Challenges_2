e39422e2c619ab90961e35909376119920f2a3e7
denamyte <denamyte@gmail.com>
2021-01-15 18:02:25 +0200

    2021-01-15: 1 problem

---
+++ Problems/Find the first occurrence of the word "the"/src/Main.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+class Main {
+
+    public static void main(String[] args) {
+        String s = new Scanner(System.in).nextLine().toLowerCase();
+        System.out.println(s.indexOf("the"));
+    }
+}
