9a94ba7646fd2af2a5a8b7a29e78e0e0ddca7ab8
denamyte <denamyte@gmail.com>
2021-02-08 13:52:31 +0200

    2021-02-08: 1 problem

---
+++ Problems/Print content/src/Main.java
@@ -0,0 +1,14 @@
+// do not remove imports
+import java.util.*;
+import java.util.function.Function;
+
+class ArrayUtils {
+    ArrayList dumbList;
+    List dumbList2;
+    Scanner dumbScanner;
+    Function dumbFunction;
+
+    static <T> String info(T[] array) {
+        return Arrays.toString(array);
+    }
+}
