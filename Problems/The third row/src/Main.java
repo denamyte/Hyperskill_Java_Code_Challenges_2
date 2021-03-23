b3a507965c85e24dd6b53a1f455fde3a53da932f
denamyte <denamyte@gmail.com>
2021-03-24 01:13:23 +0200

    2021-03-23: 2 problems

---
+++ Problems/The third row/src/Main.java
@@ -0,0 +1,7 @@
+class ArrayOperations {
+    public static void printTheThirdRow(int[][] twoDimArray) {
+        for (int i : twoDimArray[2]) {
+            System.out.printf("%d ", i);
+        }
+    }
+}
