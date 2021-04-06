df5ad08b8691c1521b50e580dd91a672884d7525
denamyte <denamyte@gmail.com>
2021-04-06 03:18:36 +0300

    2021-04-06: 2 problems

---
+++ Topics/Encapsulating object creation/Motor static factory/src/Main.java
@@ -0,0 +1,77 @@
+import java.util.Map;
+import java.util.Scanner;
+import java.util.function.BiFunction;
+
+class MotorStaticFactory {
+
+    static final Map<Character, BiFunction<String, Long, Motor>> MOTOR_MAP = Map.of(
+            'P', PneumaticMotor::new,
+            'H', HydraulicMotor::new,
+            'E', ElectricMotor::new,
+            'W', WarpDrive::new
+    );
+
+    /**
+     * It returns an initialized motor according to the specified type by the first character:
+     * 'P' or 'p' - pneumatic, 'H' or 'h' - hydraulic, 'E' or 'e' - electric, 'W' or 'w' - warp.
+     */
+    public static Motor make(char type, String model, long power) {
+        final var motorFn = MOTOR_MAP.get(Character.toUpperCase(type));
+        return motorFn == null ? null : motorFn.apply(model, power);
+    }
+}
+
+/* Do not change code below */
+class Motor {
+
+    String model;
+    long power;
+
+    public Motor(String model, long power) {
+        this.model = model;
+        this.power = power;
+    }
+}
+
+class PneumaticMotor extends Motor {
+
+    public PneumaticMotor(String model, long power) {
+        super(model, power);
+    }
+}
+
+class HydraulicMotor extends Motor {
+
+    public HydraulicMotor(String model, long power) {
+        super(model, power);
+    }
+}
+
+class ElectricMotor extends Motor {
+
+    public ElectricMotor(String model, long power) {
+        super(model, power);
+    }
+}
+
+class WarpDrive extends Motor {
+
+    public WarpDrive(String model, long power) {
+        super(model, power);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        final Scanner scanner = new Scanner(System.in);
+        final char type = scanner.next().charAt(0);
+        final String model = scanner.next();
+        final long power = scanner.nextLong();
+        final Motor motor = MotorStaticFactory.make(type, model, power);
+        if (motor == null) {
+            System.out.println(motor);
+        } else {
+            System.out.println(motor.getClass().getName() + " " + motor.model + " " + motor.power);
+        }
+    }
+}
